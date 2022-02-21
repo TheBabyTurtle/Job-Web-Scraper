cfff114c46184c5ea7ecff23a8d9aa173d1d4b29
mnuss01 <74118538+mnuss01@users.noreply.github.com>
2022-02-21 17:31:29 -0500

    Add files via upload
    Second commit of Combo.py

--- Combo.py
+++ Combo.py
@@ -1,14 +1,17 @@
 import PySimpleGUI as sg
 import requests
 from bs4 import BeautifulSoup
+
 # Sample website: https://realpython.github.io/fake-jobs/
 
 sg.theme('SandyBeach')
 
 layout = [
-    [sg.Text('Please enter specifications')],
-    [sg.Text('URL', size=(25, 1)), sg.InputText()],
-    [sg.Button("Submit"),sg.Button("Cancel")]
+    [sg.Text('Choose Website')],
+    [sg.Combo(['https://realpython.github.io/fake-jobs/', 'https://www.ms3-inc.com/careers/'])],
+    [sg.Text('Job Title', size=(8, 1)), sg.InputText()],
+    [sg.Text('Location', size=(8, 1)), sg.InputText()],
+    [sg.Button("Submit"), sg.Button("Cancel")]
 ]
 
 window = sg.Window('Web Scraper', layout)
@@ -16,7 +19,8 @@ while True:
     event, values = window.read()
     if event == "Submit":
         URL = values[0]
-
+        Job_Title = values[1]
+        Location = values[2]
         page = requests.get(URL)
 
         soup = BeautifulSoup(page.content, "html.parser")
@@ -28,12 +32,14 @@ while True:
             company_element = job_element.find("h3", class_="company")
             location_element = job_element.find("p", class_="location")
             link_element = job_element.find_all("a")
-            print(title_element.text.strip())
-            print(company_element.text.strip())
-            print(location_element.text.strip())
-            link_url = link_element[1]["href"]
-            print(f"Apply Here: {link_url}\n")
-            print()
+            if values[1] == title_element.text.strip() and values[2] == location_element.text.strip():
+                print(title_element.text.strip())
+                print(company_element.text.strip())
+                print(location_element.text.strip())
+                link_url = link_element[1]["href"]
+                print(f"Apply Here: {link_url}\n")
+                print()
+            
         break
     if event == "Cancel":
         break
