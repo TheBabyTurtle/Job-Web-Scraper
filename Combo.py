003b62b67bf223f02468302db254315db406bb2b
TheBabyTurtle <62351065+TheBabyTurtle@users.noreply.github.com>
2022-03-02 13:48:06 -0500

    Update Combo.py

--- Combo.py
+++ Combo.py
@@ -2,21 +2,59 @@ import PySimpleGUI as sg
 import requests
 from bs4 import BeautifulSoup
 
-sg.theme('SandyBeach')
 
-layout = [
-    [sg.Text('Choose Website')],
-    [sg.Combo(['https://realpython.github.io/fake-jobs/', 'https://www.ms3-inc.com/careers/',
-               'http://health.wvu.edu/healthaffairs/careers/'])],
-    [sg.Text('Job Title', size=(8, 1)), sg.InputText()],
-    [sg.Text('Location', size=(8, 1)), sg.InputText()],
-    [sg.Button("Submit"), sg.Button("Cancel")]
-]
+def inputGUI():
+    layout = [
+        [sg.Text('Choose Website')],
+        [sg.Combo(['https://realpython.github.io/fake-jobs/', 'https://www.ms3-inc.com/careers/',
+            'http://health.wvu.edu/healthaffairs/careers/'])],
+        [sg.Text('Job Title', size=(8, 1)), sg.InputText()],
+        [sg.Text('Location', size=(8, 1)), sg.InputText()],
+        [sg.Button("Submit"), sg.Button("Cancel")]
+    ]
+    window = sg.Window('Web Scraper', layout)
+    while True:
+        event, values = window.read()
+        if event == "Submit":
+            URL = values[0]
+            Job_Title = values[1]
+            Location = values[2]
+            if values[0] == 'https://realpython.github.io/fake-jobs/':
+                window.close()
+                tutorial(values)
+            if values[0] == 'https://www.ms3-inc.com/careers/':
+                window.close()
+                ms3(values)
+            if values[0] == 'http://health.wvu.edu/healthaffairs/careers/':
+                window.close()
+                health(values)
+            break
+        if event == "Cancel" or event == sg.WIN_CLOSED:
+            break
+        window.close()
 
-window = sg.Window('Web Scraper', layout)
+
+def outputGUI(elements):
+    layout = [
+        [sg.Text('Job Results')],
+        [sg.Output(key='Output', size=(100,30))],
+        [sg.Button('Continue Search'), sg.Button('Close')]
+    ]
+    window = sg.Window('Results', layout, element_justification='center').finalize()
+    window['Output'].TKOut.output.config(wrap='word')
+    for element in elements:
+        print(element)
+    while True:
+        event, values = window.read()
+        if event == 'Continue Search':
+            break
+        if event == 'Close' or event == sg.WIN_CLOSED:
+            break
+    window.close()
 
 
-def tutorial():
+def tutorial(values):
+    elements = []
     url = "https://realpython.github.io/fake-jobs/"
     page = requests.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
@@ -28,15 +66,16 @@ def tutorial():
         location_element = job_element.find("p", class_="location")
         link_element = job_element.find_all("a")
         if values[1] == title_element.text.strip() or values[2] == location_element.text.strip():
-            print(title_element.text.strip())
-            print(company_element.text.strip())
-            print(location_element.text.strip())
+            elements.append(title_element.text.strip())
+            elements.append(company_element.text.strip())
+            elements.append(location_element.text.strip())
             link_url = link_element[1]["href"]
-            print(f"Apply Here: {link_url}\n")
-            print()
+            elements.append(f"Apply Here: {link_url}\n")
+    outputGUI(elements)
 
 
-def ms3():
+def ms3(values):
+    elements = []
     url = "https://www.ms3-inc.com/careers/"
     page = requests.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
@@ -46,49 +85,31 @@ def ms3():
         title_element = job_element.find("div", class_="skill-title")
         link_element = job_element.find("a")
         if values[1] == title_element.text.strip():
-            print(title_element.text.strip())
+            elements.append(title_element.text.strip())
             link_url = link_element["href"]
-            print(f"Apply Here: {link_url}\n")
+            elements.append(f"Apply Here: {link_url}\n")
+    outputGUI(elements)
 
 
-def health():
+def health(values):
     url = "http://health.wvu.edu/healthaffairs/careers/"
     page = requests.get(url)
     counter = 0
     soup = BeautifulSoup(page.content, "html.parser")
-    pieces = []
-    piece = []
+    elements = []
     results = soup.find(id="content")
     target = results.find(class_="page-primary rich-text")
     for child in target.children:
         if child.name == "h4":
-            piece.append(child.text.strip())
+            elements.append(child.text.strip())
             while True:
                 child = child.nextSibling
                 if child.name == "h4" or child.name == "h3":
-                    pieces += piece
-                    counter += 1
-                    piece.clear()
                     break
                 elif child.name == "p":
-                    piece.append(child.text.strip())
-    for bit in pieces:
-        print(bit)
+                    elements.append(child.text.strip())
+    outputGUI(elements)
 
 
-while True:
-    event, values = window.read()
-    if event == "Submit":
-        URL = values[0]
-        Job_Title = values[1]
-        Location = values[2]
-        if values[0] == 'https://realpython.github.io/fake-jobs/':
-            tutorial()
-        if values[0] == 'https://www.ms3-inc.com/careers/':
-            ms3()
-        if values[0] == 'http://health.wvu.edu/healthaffairs/careers/':
-            health()
-        break
-    if event == "Cancel" or event == sg.WIN_CLOSED:
-        break
-window.close()
+sg.theme('SandyBeach')
+inputGUI()
