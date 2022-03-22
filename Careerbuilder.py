b4520c1c0fd6b1136fb6d5bc1c05d03d16781d43
TheBabyTurtle <62351065+TheBabyTurtle@users.noreply.github.com>
2022-03-22 13:46:31 -0400

    Update Careerbuilder.py

--- Careerbuilder.py
+++ Careerbuilder.py
@@ -13,13 +13,16 @@ for job_element in job_elements:
     job_description = job_element.findAll("div", class_="block")
     job_link = job_element.find("a")
     print("\n" + "Job Title: " + title_element.text.strip())
-    print("Company Name: " + job_details[0].text.strip())
-    print("Location: " + job_details[1].text.strip())
-    print("Part/Full-time: " + job_details[2].text.strip())
+    if len(job_details) == 3:
+        print("Company Name: " + job_details[0].text.strip())
+        print("Location: " + job_details[1].text.strip())
+        print("Part/Full-time: " + job_details[2].text.strip())
+    elif len(job_details) == 2:
+        print("Location: " + job_details[0].text.strip())
+        print("Part/Full-time: " + job_details[1].text.strip())
     print("Description: " + job_description[0].text.strip())
     if job_description[1].text.strip() != "":
         print("Salary/Pay: " + job_description[1].text.strip())
     else:
         print("No salary/pay given.")
     print("Apply Here: https://www.careerbuilder.com/" + job_link["href"])
-    
