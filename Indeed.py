5a22fb84edbda5fd4e6b673f84ff0b3889662bbf
TheBabyTurtle <62351065+TheBabyTurtle@users.noreply.github.com>
2022-03-24 16:33:25 -0400

    Update Indeed.py

--- Indeed.py
+++ Indeed.py
@@ -1,12 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://www.indeed.com/"
+links = []
+URL = "https://www.indeed.com/jobs?q=Network%20Administrator"
 page = requests.get(URL)
 soup = BeautifulSoup(page.content, "html.parser")
-results = soup.find(id="jobsearch-HomePage")
-job_elements = results.find_all("div", class_="serpLinking-Column-Entry")
-for job_element in job_elements:
-    title_element = job_element.find("a")
-    print(title_element.text.strip())
-    # print(job_element.prettify(), end="\n"*2)
+targets = soup.findAll('a', class_='tapItem')
+for target in targets:
+    links.append(target['href'])
+for link in links:
+    print(link)
+    new_page = requests.get('https://www.indeed.com' + link)
+    new_soup = BeautifulSoup(new_page.content, 'html.parser')
+    position_name = new_soup.find('h1', class_='icl-u-xs-mb--xs icl-u-xs-mt--none jobsearch-JobInfoHeader-title')
+    company_name = new_soup.find('div', class_='jobsearch-CompanyReview--heading')
+    if company_name is None:
+        company_name = new_soup.find('div', class_="jobsearch-InlineCompanyRating icl-u-xs-mt--xs jobsearch-DesktopStickyContainer-companyrating")
+        for child in company_name.children:
+            if child.text.strip() != '':
+                company_name = child
+                break
+    description = new_soup.find('div', id='jobDescriptionText')
+    print(position_name.text.strip())
+    print(company_name.text.strip())
+    print(description.text.strip())
