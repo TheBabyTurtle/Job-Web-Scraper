fae0b1f97eaa293c49c50a8bb48ff02ec0713e41
mnuss01 <74118538+mnuss01@users.noreply.github.com>
2022-04-05 10:36:58 -0400

    Add files via upload
    Now prints title, department, agency, location, description, and link

--- usajobs.py
+++ usajobs.py
@@ -1,15 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://www.usajobs.gov/Search/Results?j=1550#"
+URL = "https://www.usajobs.gov/Search/ExploreOpportunities?Series=1550"
 page = requests.get(URL)
 soup = BeautifulSoup(page.content, "html.parser")
 results = soup.find(id="main-content")
-# print(results.prettify())
-# Only shows government positions in high demand
+# Only shows Computer Science government positions
 
-job_elements = results.find_all("li", class_="usajobs-search-no-params-highlight__list-item")
+job_elements = results.find_all("div", class_="usajobs-search-result--card")
 for job_element in job_elements:
-    title_element = job_element.find("a")
-    print(title_element.text.strip())
+    title_element = job_element.find("h3", class_="usajobs-search-result__title")
+    department_element = job_element.find("h4", class_="usajobs-search-result__department")
+    agency_element = job_element.find("h5", class_="usajobs-search-result__agency")
+    location_element = job_element.find("h4", class_="usajobs-search-result__location")
+    description_element = job_element.find("p", class_="usajobs-search-result__multi-line")
+    link_element = job_element.find("a")
+    print("Job title: " + title_element.text.strip())
+    print("Department: " + department_element.text.strip())
+    print("Agency: " + agency_element.text.strip())
+    print("Location: " + location_element.text.strip())
+    print("Description: " + description_element.text.strip())
+    print("Apply here: " + link_element["href"] + "\n")
     # print(job_element.prettify(), end="\n"*2)
